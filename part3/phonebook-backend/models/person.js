ed87220655f9120bd152f48b68ed4d3c9b3ffc43
imagicw <wyw310@gmail.com>
2020-12-09 16:57:05 +0800

    part 3 done

---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect
+(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+)
+.then(result =>
+{
+  console.log('MongoDB connected')
+})
+.catch((error)=>
+{
+  console.log('Connecting MongoDB Failed: ', error.message)
+})
+
+const phonebookSchema = new mongoose.Schema(
+  {
+    name: String,
+    number: String,
+  }
+)
+
+phonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', phonebookSchema)
